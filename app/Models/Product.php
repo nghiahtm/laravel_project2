51e5a9999a224779f82caf51e80bc7ef73df54ba
hoang <107757542+hkhanhdev@users.noreply.github.com>
2024-06-03 16:31:11 +0700

    commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,7 +11,7 @@ class Product extends Model
 
     public $timestamps = false;
     protected $fillable = ["id","manufacturer_id","name","selling_price","image_url",
-        "ram","display","processor","original_price","display","storage"];
+        "ram","display","processor","original_price","display","storage",'os'];
     public  function manufacturers()
     {
         return $this->belongsTo(Manufacturers::class);
